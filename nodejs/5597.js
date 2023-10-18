a8013afc7bad1c9d06d20be24311e4d95cd1b646
BOKYUNGGG <113874167+BOKYUNGGG@users.noreply.github.com>
2023-10-18 19:58:27 +0900

    2023-10-18

--- nodejs/5597.js
+++ nodejs/5597.js
@@ -1,7 +1,18 @@
 const inputs = require('fs').readFileSync('input.txt').toString().split('\n').map(x=>Number(x))
-// students = []
-const students = Array.from({length:inputs.length}, ()=>0)
-console.log(students)
-for(let i=0;i<inputs.length;i++){
+// students = [ false, false, ... ]
+const students = Array.from({length:30}, ()=>false)
 
+for(let i=0;i<inputs.length;i++){
+    students[inputs[i]-1] = true
+}
+console.log(students)
+let count = 0
+for(let [i,v] of students.entries()){
+    if(count == 2) {
+        break
+    }
+    if(!v) {
+        console.log(i+1)
+        count += 1
+    }
 }
