62f7e1fc2852a4632330da9c7146b9f6a4254e2a
BOKYUNGGG <113874167+BOKYUNGGG@users.noreply.github.com>
2023-11-02 23:44:13 +0900

    2023-11-02

---
+++ nodejs/2675.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('input.txt').toString().trim().split('\n')
+
+const T = Number(input.shift())
+
+for(let i=0;i<T;i++){
+    const [R, S] = input[i].split(' ')
+    let answer = []
+    for(let j=0; j<S.length; j++){
+        answer.push(S[j].repeat(R))
+    }
+    console.log(answer.join(''))
+}
---
+++ nodejs/2908.js
@@ -0,0 +1,13 @@
+let [A,B] = require('fs').readFileSync('/dev/stdin').toString().split(' ')
+
+let a = []
+let b = []
+for(let i=0;i<A.length;i++){
+    a[i] = A[i]
+    b[i] = B[i]
+}
+
+A = Number(a.reverse().join(''))
+B = Number(b.reverse().join(''))
+if(A<B) console.log(B)
+else console.log(A)
