a2c4108ba31abfa433761137bc929f41de3a6bfb
Bo Kyung <113874167+BOKYUNGGG@users.noreply.github.com>
2023-11-07 22:55:53 +0900

    2023-11-7

---
+++ nodejs/2738.js
@@ -0,0 +1,30 @@
+const input = `3 3
+1 1 1
+2 2 2
+0 1 0
+3 3 3
+4 4 4
+5 5 100`.split('\n')
+
+
+const [N,M]= input.shift().split(' ').map(x=>Number(x))
+
+let arr1 = []
+let arr2 = []
+for(let i=0;i<N;i++){
+    const columns = input.shift().split(' ').map(x=>Number(x))
+    arr1.push(columns)
+}
+for(let i=0;i<N;i++){
+    const columns = input.shift().split(' ').map(x=>Number(x))
+    arr2.push(columns)
+}
+
+
+for(let i=0;i<N;i++){
+    let row = []
+    for(let j=0;j<M;j++){
+        row.push(arr1[i][j] + arr2[i][j])
+    }
+    console.log(row.join(' '))
+}
